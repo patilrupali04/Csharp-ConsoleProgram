154f1355563c1fbbbe441d1f4c70f0fc7c4c173d
Rupali Patil <125479555+rupspatil@users.noreply.github.com>
2023-02-18 19:03:19 +0530

    Add files via upload

---
+++ IfElse.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    internal class IfElse
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter number");
+            int UserNumber = int.Parse(Console.ReadLine());
+            if (UserNumber == 1)
+            {
+                Console.WriteLine("Your number is One");
+            }
+            else if (UserNumber == 2)
+            {
+                Console.WriteLine("Your number is Two");
+            }
+            else if (UserNumber == 3)
+            {
+                Console.WriteLine("Your number is Three");
+            }
+            else
+            {
+                Console.WriteLine("Your number is not between 1 and 3");
+            }
+
+        }
+
+    }
+}

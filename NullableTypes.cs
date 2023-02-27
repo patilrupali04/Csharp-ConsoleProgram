9e6a6af16971d49cd2b9ac361a4fc284d15254f9
Rupali Patil <125479555+rupspatil@users.noreply.github.com>
2023-02-27 14:20:34 +0530

    Add files via upload
    value type is non nullable but use ? for nullable

---
+++ NullableTypes.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp5
+{
+    internal class NullableTypes
+    {
+        static void Main(string[] args)
+        {
+            bool ? AreYouMajor = null;
+            if (AreYouMajor == true)
+            {
+                Console.WriteLine("User is Major");
+            }
+            else if(AreYouMajor == false)
+            {
+                Console.WriteLine("User is not Major");
+            }
+            else
+            {
+                Console.WriteLine("User Did not answer the questions");
+            }
+        }
+    }
+}

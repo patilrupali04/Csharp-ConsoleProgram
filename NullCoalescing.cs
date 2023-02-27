7eaa848a9edb7f44b3c89c521c9fb9c63ca9cdb1
Rupali Patil <125479555+rupspatil@users.noreply.github.com>
2023-02-27 14:21:30 +0530

    Add files via upload

---
+++ NullCoalescing.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp6
+{
+    internal class NullCoalescing
+    {
+        static void Main(string[] args)
+        {
+            int ? TicketOnSale = 100;
+            int AvalaibleTickets = TicketOnSale ?? 0;
+            Console.WriteLine("AvailableTickets {0}", AvalaibleTickets);
+
+        }
+    }
+}

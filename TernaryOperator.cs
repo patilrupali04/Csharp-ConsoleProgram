602d8f0dbbf1e0364f529951e67f43be718bc2ad
Rupali Patil <125479555+rupspatil@users.noreply.github.com>
2023-02-27 13:29:51 +0530

    Add files via upload

---
+++ TernaryOperator.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int Number = 15;
+            //int AnotherNumber = 10;
+            //if(Number==15 && AnotherNumber == 10)
+            //{
+            //    Console.WriteLine("Hello");
+            //}
+            //else if(Number==15 || AnotherNumber == 20)
+            //{
+            //    Console.WriteLine("Hi");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("Never");
+            //}
+
+            bool IsNumber10;
+            IsNumber10=Number==10?true:false;
+            Console.WriteLine("Number is {0}", IsNumber10);
+        }
+    }
+}

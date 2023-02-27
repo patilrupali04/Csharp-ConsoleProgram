f2b8aa42586cc7c331f7b36a7a4eb58df21a3456
Rupali Patil <125479555+rupspatil@users.noreply.github.com>
2023-02-27 13:25:21 +0530

    Add files via upload
    operator

---
+++ ArithmeticOperator.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleAPP3
+{
+    internal class ArithmeticOperator
+    {
+        static void Main(string[] args)
+        {
+            int Numerator = 5;
+            int denominator = 1;
+            int result = Numerator / denominator;
+            Console.WriteLine("Result = {0}", result);
+        }
+    }
+}

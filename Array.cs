0c0bafa0526723f9b0c5c0e46a25a4d5e6f56b9e
Rupali Patil <125479555+rupspatil@users.noreply.github.com>
2023-02-28 00:12:33 +0530

    Add files via upload
    Advantage of Array: Array is a Strongly typed
    Disadvantage of Array: Array size cannot grow once initialized

---
+++ Array.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp8
+{
+    internal class Array
+    {
+        static void Main(string[] args)
+        {
+            //Array is collection of Similar Datatypes
+
+            int[] EvenNum = new int[4];
+            EvenNum[0] = 2;        //Array is Strongly Typed
+            EvenNum[1] = 4;
+            EvenNum[2] = 6;
+            EvenNum[3] = 8;
+          //  EvenNum[4] = 10;     Disadvantage-Array cannot grows once initialized  
+
+            Console.WriteLine("EvenNumber is : {0}", EvenNum[2]);
+        }
+    }
+}

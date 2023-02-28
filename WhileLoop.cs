150672c5ad538753fce4acc53f38ab0af920f96d
Rupali Patil <125479555+patilrupali04@users.noreply.github.com>
2023-02-28 23:43:19 +0530

    Add files via upload
    In While loop check condition first if condition is true then statement within loop executed .

---
+++ WhileLoop.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp11
+{
+    internal class WhileLoop
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please Enter Target Number");
+            int TargetNum = Convert.ToInt32(Console.ReadLine());
+            int Value = 0;
+            while (Value <= TargetNum)
+            {
+                Console.WriteLine(Value);
+                Value=Value+2;
+                    
+            }
+        }
+    }
+}

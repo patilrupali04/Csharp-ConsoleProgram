74ef4aa321e3835cfe8d8e42573cb94ef8434faf
Rupali Patil <125479555+rupspatil@users.noreply.github.com>
2023-02-28 14:42:39 +0530

    Add files via upload

---
+++ Switch.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp9
+{
+    internal class Switch
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter a Number");
+            int UserNumber=Convert.ToInt32(Console.ReadLine()); 
+            switch (UserNumber)
+            {
+                case 1:
+                    Console.WriteLine("This is a UserNumber one");
+                    break;
+                    
+                case 2:
+                    Console.WriteLine("This is a UserNumber Two");
+                    break;
+                case 3:
+                    Console.WriteLine("This is a UserNumber Three");
+                    break;
+                default:
+                    Console.WriteLine("This is not UserNumber one, Two & Three");
+                    break;
+
+            }
+        }
+    }
+}

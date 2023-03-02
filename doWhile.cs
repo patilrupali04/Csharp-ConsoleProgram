27ee39e7895fd85267e2dd6b8b86c7e9c8ef2eed
Rupali Patil <125479555+patilrupali04@users.noreply.github.com>
2023-03-02 15:14:12 +0530

    Add files via upload
    While loop check the condition first but do while loop check the condition at the end of the loop, Do while loop executed at least once but while loop not.

---
+++ doWhile.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp12
+{
+    internal class doWhile
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter your Target");
+            int UserChoice = int.Parse(Console.ReadLine());
+            int fixValue = 0;
+            do
+            {
+                fixValue=fixValue+1;
+                Console.WriteLine("fixvalue {0}", fixValue);
+                
+               
+            } while (fixValue<=UserChoice);   //do while loop check the condition at the end of the loop
+            
+            //do loop is guaranted to execute at least once
+        }
+    }
+}
